5de2fdc21715a03721dc01fd8e3f29352e796ac9
eleve <eleve@z210>
2019-12-03 11:52:17 +0100

    please work baby

--- src/Controller/AffecterController.php
+++ src/Controller/AffecterController.php
@@ -1,10 +1,8 @@
 <?php
 
 namespace App\Controller;
-
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use App\Entity\Affecter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class AffecterController extends AbstractController {
 
@@ -12,12 +10,12 @@ class AffecterController extends AbstractController {
      * @Route("/affecter", name="affecter")    
      */
     public function index() {
-        $affecter = new Affecter();
-        $form = $this->createFormBuilder($affecter)
-                        ->add('user', EntityType::class, array('class' => 'App\Entity\User', 'choice_label' => 'nom'))
-                        ->add('logiciel', EntityType::class, array('class' => 'App\Entity\Logciel', 'choice_label' => 'nom'))
-                        ->add('save', SubmitType::class, array('label' => 'Ajouter'))->getForm();
-        return $this->render('affecter/index.html.twig', ['form' => $form->createView(),]);
+       
+   
+        
+       return $this->render('affecter/index.html.twig', [
+            'controller_name' => 'FichierController',
+        ]);
     }
 
 }
